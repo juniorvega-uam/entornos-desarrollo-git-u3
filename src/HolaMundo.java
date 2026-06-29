86dc138e8a8efefd5a227e3392bfa0b21902a729
Junior Enrique Vega Solano <junior.vega@uamcr.net>
2026-06-28 23:46:20 -0600

    feat: añadir saludo personalizado

--- src/HolaMundo.java
+++ src/HolaMundo.java
@@ -9,6 +9,6 @@ public class HolaMundo {
         System.out.print("Introduce tu nombre: ");
         String nombre = sc.nextLine();
 
-        System.out.println("Hola " + nombre);
+        System.out.println("Version Rama");
     }
 }
