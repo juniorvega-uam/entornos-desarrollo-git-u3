fe6abd1689aa990d8f8dcdb21df5fba9a18c1ff7
Junior Enrique Vega Solano <junior.vega@uamcr.net>
2026-06-28 23:34:10 -0600

    feat: ampliar mensaje de bienvenida

--- src/HolaMundo.java
+++ src/HolaMundo.java
@@ -2,6 +2,6 @@ public class HolaMundo {
 
     public static void main(String[] args) {
 
-        System.out.println("Hola, Tecnovía!");
+        System.out.println("Bienvenido al proyecto Tecnovía");
     }
 }
